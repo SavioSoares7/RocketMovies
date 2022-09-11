27d618f7d99dfb345b51fa74e119f12bde496d5f
Savio <dsaviosnogueira@gmail.com>
2022-09-11 16:24:43 -0300

    Finished page MoviePreview

--- RocketMovies/src/page/MoviePreview/style.js
+++ RocketMovies/src/page/MoviePreview/style.js
@@ -1,1 +1,42 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 1080px;
+  margin: 0 auto;
+  padding-bottom: 20px;
+`;
+
+export const ContainerUser = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  margin-bottom: 40px;
+
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+  > div#user {
+    > img {
+      height: 16px;
+      width: 16px;
+      border-radius: 50%;
+    }
+  }
+  > div#time {
+    > svg {
+      color: ${({ theme }) => theme.COLORS.FT_PINK};
+    }
+  }
+`;
+
+export const ContainerText = styled.div`
+  margin-top: 40px;
+
+  > p {
+    margin-bottom: 20px;
+    text-align: justify;
+  }
+`;
